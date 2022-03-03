95bce0928ff705bea18ff63f7d0032ac2247003c
Ekaterina Samokhina <samohina.katerina@yandex.ru>
2022-03-04 02:51:47 +0300

    final task done

--- task_final/solution.js
+++ task_final/solution.js
@@ -26,7 +26,6 @@
 //       },
 //       "goods": [
 //         {
-//           "title": "Пицца",
 //           "count": 2
 //         }
 //      ]
@@ -34,20 +33,24 @@
 // }
 
 function sendRequest(name, phone, address, goods, sum) {
-    let data = {goods: [], order: {}};
-
-    let countOfGoods = goods.length;
-
-    for (let i = 0; i <= countOfGoods; i += 1) {
-        data.goods.push(goods[i].title);
-    }
-
-    data.order.address = address;
-    data.order.sum = name + phone + address + goods + sum;
-
-    data.client = 'Иван';
-
-    let jsonData = JSON.stringify(data);
-
-    return jsonData;
-}
+ 
+    let data = {
+        client: `${name} ${phone}`,
+        order: {address, sum},
+        goods: []
+      };
+  
+      data.order.address = "ул. " + address.street + ", дом " + address.house + ", " + address.entrance + " подъезд, " + address.floor + " этаж, кв " + address.flat;
+  
+      let countOfGoods = goods.length-1;
+      let object = {};
+  
+      for (let i = 0; i <= countOfGoods; i += 1) {
+        object = {title: goods[i].title, count: goods[i].count}  
+        data.goods.push (object);
+      };
+      
+         let jsonData = JSON.stringify({data});
+  
+      return jsonData;
+    };
